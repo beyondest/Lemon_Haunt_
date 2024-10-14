84331132527d1598e6fe7d452f42221662c3ed3d
liyuxuan <liyuxuan12345678@outlook.com>
2024-10-14 08:45:11 -0400

    finish logic of getting caught by gargoyle.

--- Assets/UnityTechnologies/Scripts/GameEnding.cs
+++ Assets/UnityTechnologies/Scripts/GameEnding.cs
@@ -1,15 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
+/// <summary>
+/// GameEnding is used to trigger the end of the level and quit the game.
+/// </summary>
 public class GameEnding : MonoBehaviour
 {
     public float fadeDuration = 1f;
     public float displayDuration = 1f;
     public GameObject player;
     public CanvasGroup exitBackgroundImageCanvasGroup;
+    public CanvasGroup caughtBackgroundImageCanvasGroup;
 
     bool m_IsPlayerAtExit = false;
+    bool m_IsPlayerCaught = false;
     float m_Timer;
 
     void OnTriggerEnter(Collider other)
@@ -19,21 +25,42 @@ public class GameEnding : MonoBehaviour
             m_IsPlayerAtExit = true;
         }
     }
+    /// <summary>
+    /// Called when the player is caught by the Gargoyle. Will reload the scene.
+    /// </summary>
+    public void CaughtPlayer()
+    {
+        m_IsPlayerCaught = true;
+    }
     void Update()
     {
         if (m_IsPlayerAtExit)
         {
-            EndLevel();
+            EndLevel(exitBackgroundImageCanvasGroup, false);
+        }
+        else if (m_IsPlayerCaught)
+        {
+            EndLevel(caughtBackgroundImageCanvasGroup,true);
         }
     }
-    void EndLevel()
+    void EndLevel(CanvasGroup imageCanvasGroup, bool doRestart)
     {
         m_Timer += Time.deltaTime;
-        exitBackgroundImageCanvasGroup.alpha = m_Timer / fadeDuration;      // alpha will over 1???
-        if(m_Timer > fadeDuration + displayDuration)
+        imageCanvasGroup.alpha = m_Timer / fadeDuration;      // alpha will over 1???
+
+        if (m_Timer > fadeDuration + displayDuration)
         {
-            Application.Quit();
+            if (doRestart)
+            {
+                SceneManager.LoadScene(0);
+            }
+            else
+            {
+                Application.Quit();
+            }
         }
     }
+
+
     
 }
--- Assets/UnityTechnologies/Scripts/Observer.cs
+++ Assets/UnityTechnologies/Scripts/Observer.cs
@@ -4,16 +4,43 @@ using UnityEngine;
 
 public class Observer : MonoBehaviour
 {
-    public Transform player;
-    // Start is called before the first frame update
-    void Start()
+    public Transform player;        // Use transform instead of gameobject to make it easier to determine clear line
+    public GameEnding gameEnding;
+    bool m_IsPlayerInRange;
+
+    void OnTriggerEnter(Collider other)
     {
-        
+        if (other.transform == player )
+        {
+            m_IsPlayerInRange = true;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnTriggerExit(Collider other)
     {
-        
+        if (other.transform == player)
+        {
+            m_IsPlayerInRange = false;
+        }
     }
+
+    private void Update()
+    {
+        if ( m_IsPlayerInRange )
+        {
+            // transform.postion is the position of the observer, player.position is the position of the feet of player, so add up
+            Vector3 direction = player.position - transform.position + Vector3.up;
+            Ray ray = new Ray(transform.position, direction);
+            RaycastHit raycastHit;
+            if ( Physics.Raycast(ray, out raycastHit ))
+            {
+                if ( raycastHit .collider.transform  == player)
+                {
+                    gameEnding.CaughtPlayer();
+                }
+            }
+        }
+    }
+
+
 }
